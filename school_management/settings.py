00bad6d1d7e7c08f196b2a9ab0b4d8ee20894419
abdul <abdul.aits004@gmail.com>
2025-10-15 17:38:08 +0530

    add base

--- school_management/settings.py
+++ school_management/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'assets/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'assets',
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
